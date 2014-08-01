1bbfef4f5e3f9b57cf8dd02a0eb7025f01f1150f
Christian Boxdörfer <christian.boxdoerfer@posteo.de>
2014-08-01 08:58:53 +0200

    add support for old gtk versions

--- rg_control.c
+++ rg_control.c
@@ -111,6 +111,23 @@ replaygain_control_apply_settings (int playorder)
     }
 }
 
+#if !GTK_CHECK_VERSION(2,14,0)
+#define gtk_widget_get_window(widget) ((widget)->window)
+#define gtk_dialog_get_content_area(dialog) (dialog->vbox)
+#define gtk_dialog_get_action_area(dialog) (dialog->action_area)
+#endif
+
+#if !GTK_CHECK_VERSION(2,18,0)
+void
+gtk_widget_get_allocation (GtkWidget *widget, GtkAllocation *allocation) {
+    (allocation)->x = widget->allocation.x;
+    (allocation)->y = widget->allocation.y;
+    (allocation)->width = widget->allocation.width;
+    (allocation)->height = widget->allocation.height;
+}
+#define gtk_widget_set_can_default(widget, candefault) {if (candefault) GTK_WIDGET_SET_FLAGS (widget, GTK_CAN_DEFAULT); else GTK_WIDGET_UNSET_FLAGS(widget, GTK_CAN_DEFAULT);}
+#endif
+
 gboolean
 on_button_config (void *userdata)
 {
